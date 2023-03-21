9f768e0d2e3c5729e261a141763c66c3aba598ed
sushilQA <101033577+sushilQA@users.noreply.github.com>
2023-03-21 17:18:05 +0530

    hello

--- src/test/java/org/testing/TestScripts/MSBackendGateway.java
+++ src/test/java/org/testing/TestScripts/MSBackendGateway.java
@@ -26,6 +26,7 @@ public class MSBackendGateway {
 		userModule.usersModule(properties.getProperty("In1"),"Live");
 		extentReports.endTest(extentTest);
 		extentReports.flush();
+		System.out.println();
 	}
 	
 	@Test(enabled=false , priority = 2)
